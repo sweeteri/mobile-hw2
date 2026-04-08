b84a4edde0d57d98ac38d9f286aef570a6ad690f
Kate <2005kpetrova@gmail.com>
2026-04-08 12:09:02 +0300

    remove deprecated course screen files

--- composeApp/src/commonMain/kotlin/com/sweeteri/stepikclient/presentation/course/CourseDetailUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.sweeteri.stepikclient.presentation.course
-
-import com.sweeteri.stepikclient.data.local.model.CourseDetail
-
-sealed interface CourseDetailUiState {
-    object Loading : CourseDetailUiState
-    data class Success(val course: CourseDetail) : CourseDetailUiState
-    data class Error(val throwable: Throwable) : CourseDetailUiState
-}
