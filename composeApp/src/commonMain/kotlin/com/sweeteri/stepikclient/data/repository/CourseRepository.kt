d08c1a1b572c5147063c79445bd4621c5b8fb49f
Kate <2005kpetrova@gmail.com>
2026-04-08 12:11:56 +0300

    Refactor coursedetail, courses repositories

--- composeApp/src/commonMain/kotlin/com/sweeteri/stepikclient/data/repository/CourseRepository.kt
+++ composeApp/src/commonMain/kotlin/com/sweeteri/stepikclient/data/repository/CourseRepository.kt
@@ -1,8 +1,15 @@
 package com.sweeteri.stepikclient.data.repository
 
 import com.sweeteri.stepikclient.data.local.model.CourseDetail
+import com.sweeteri.stepikclient.data.local.model.CourseReview
+import com.sweeteri.stepikclient.data.local.model.ReviewPage
 
 interface CourseDetailRepository {
     suspend fun getCourseDetail(courseId: Int): Result<CourseDetail>
     suspend fun enroll(courseId: Int, userId: Int): Result<Boolean>
+    suspend fun getReviews(
+        courseId: Int,
+        page: Int,
+        score: Int?
+    ): Result<ReviewPage>
 }
